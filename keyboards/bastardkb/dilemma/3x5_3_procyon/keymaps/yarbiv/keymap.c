29be53974c861ce8fe68410806ad069e6f576fa2
Yoav Arbiv <yarbiv@plaid.com>
2025-12-14 21:47:50 -0500

    fixes

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/yarbiv/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/yarbiv/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //	  		-------------------------------------------------			-------------------------------------------------
 			XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,             XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,
 //	  		-------------------------------------------------			-------------------------------------------------
-			XXXXXX,   XXXXXX,   KC_BTN2,  KC_BTN1,  XXXXXX,             XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,
+			XXXXXX,   XXXXXX,   MS_BTN2,  MS_BTN1,  XXXXXX,             XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,
 //	  		-------------------------------------------------			-------------------------------------------------
 			XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,             XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,   XXXXXX,
 //	  		-------------------------------------------------			-------------------------------------------------
